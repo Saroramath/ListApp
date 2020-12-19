5475fd3fba796dc1dcc4656de6284af7c2de12c9
Shivam <shivam.iiserm@gmail.com>
2020-12-19 13:53:55 -0330

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>New App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
